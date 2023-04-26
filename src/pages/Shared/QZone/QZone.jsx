d3000a1ee569aea79fda131c4e50c794bbfcac95
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-26 23:29:33 +0600

    Right side navigation

---
+++ src/pages/Shared/QZone/QZone.jsx
@@ -0,0 +1,16 @@
+import qzone1 from "../../../assets/qZone1.png";
+import qzone2 from "../../../assets/qZone2.png";
+import qzone3 from "../../../assets/qZone3.png";
+
+const QZone = () => {
+  return (
+    <div className="bg-secondary text-center my-4 py-4">
+      <h4>Q-Zone</h4>
+      <img src={qzone1} alt="" />
+      <img src={qzone2} alt="" />
+      <img src={qzone3} alt="" />
+    </div>
+  );
+};
+
+export default QZone;
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,7 +1,42 @@
+import { Button, ListGroup } from "react-bootstrap";
+import {
+  FaGoogle,
+  FaFacebook,
+  FaGithub,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+import bg from "../../../assets/bg.png";
+import QZone from "../QZone/QZone";
+
 const RightNav = () => {
   return (
     <div>
-      <h3>Right Nav</h3>
+      <h4 className="text-center">Login With</h4>
+      <div className="d-flex flex-column">
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Google
+        </Button>
+        <Button variant="outline-secondary">
+          <FaGithub /> GitHub
+        </Button>
+      </div>
+      <div>
+        <h4 className="text-center">Find us on</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone />
+      <img src={bg} alt="" />
     </div>
   );
 };
