fabcbe670ebea8bd2f2e9053471307ae7dedf8ed
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-26 22:24:59 +0600

    react marquee added for latest news animation

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,11 @@
 import moment from "moment/moment";
 import logo from "../../../assets/logo.png";
+import { Button, Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
@@ -11,7 +13,15 @@ const Header = () => {
         </p>
         <p>{moment().format("dddd, MMMM D, YYYY")}</p>
       </div>
-    </div>
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-danger">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum odio
+          eaque reiciendis molestias quis necessitatibus, aperiam voluptas ad
+          sapiente quibusdam!........
+        </Marquee>
+      </div>
+    </Container>
   );
 };
 
