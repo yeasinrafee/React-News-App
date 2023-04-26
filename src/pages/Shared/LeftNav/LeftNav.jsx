596ede7068b3a34ae983af4b48f48ef952a7e02c
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-26 23:53:25 +0600

    Fetching data from backend server

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 const LeftNav = () => {
+  const [catagories, setCatagories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/catagories")
+      .then((res) => res.json())
+      .then((data) => setCatagories(data))
+      .catch((error) => console.error(error));
+  });
+
   return (
     <div>
-      <h3>Left Nav</h3>
+      <h3>All Categories</h3>
     </div>
   );
 };
