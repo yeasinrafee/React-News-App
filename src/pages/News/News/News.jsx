76727ed136fde20901f0264ad66488788b92ec66
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-28 15:16:24 +0600

    News are shown to ui

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,11 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link, useLoaderData } from "react-router-dom";
 
 const News = () => {
   const news = useLoaderData();
+  const { _id, title, details, image_url, category_id } = news;
   return (
-    <div>
-      <h2>News is coming soon....</h2>
-    </div>
+    <Card>
+      <Card.Img variant="top" src={image_url} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{details}</Card.Text>
+        <Link to={`/category/${category_id}`}>
+          <Button variant="danger">
+            <FaArrowLeft /> All news in this Category
+          </Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 };
 
